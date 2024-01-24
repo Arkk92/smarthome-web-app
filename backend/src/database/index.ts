d86b28a89a0e8083ba9d4c31b6ad95b8aeecfaf1
Marc Lopez <marclopezgarcia@gmail.com>
2024-01-24 11:49:23 +0100

    Fixed mongoose connect code error and reactivated the import.

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,16 +1,22 @@
-import mongoose from 'mongoose'
-
+import mongoose from "mongoose";
 
 class Database {
-   static connect(){
-        mongoose.connect(process.env.MONGO_URL || '', { dbName: process.env.DATABASE, useNewUrlParser: true, useUnifiedTopology: true }, () => {
-            if(mongoose.connection.readyState === 1) {
-              console.error('Database connected successfuly');
-            } else {
-              console.log('Could not connect to database');
-            }
-          });
-    }
+  static connect() {
+    const options = 
+    mongoose.connect(
+      process.env.MONGO_URL || "",
+      {
+        dbName: process.env.DATABASE,
+      }).then(
+        () => {
+          if (mongoose.connection.readyState === 1) {
+            console.error("Database connected successfuly");
+          } else {
+            console.log("Could not connect to database");
+          }
+        }
+      );
+  }
 }
 
 export default Database;
