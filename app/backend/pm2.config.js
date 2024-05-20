e397accc84df7d2cafe103cb2c88b87a35cea597
Marc Lopez <marclopezgarcia@gmail.com>
2024-05-20 14:42:15 +0200

    Corrected pm2 script to execute

--- app/backend/pm2.config.js
+++ app/backend/pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'Smart Home Project',
-            script: './dist/app/index.js',
+            script: './dist/app/presentation/express/settings/server.js',
             node_args: '-r ts-node/register -r tsconfig-paths/register'
         },
     ],
