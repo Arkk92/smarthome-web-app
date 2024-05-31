9e0347e24a596d8d7e82a174f70cc35f65884c50
Marc Lopez <marclopezgarcia@gmail.com>
2024-05-31 09:34:54 +0200

    Added openapi and loading of swagger docs from yamls.

--- app/backend/app/presentation/express/docs/swagger.ts
+++ app/backend/app/presentation/express/docs/swagger.ts
@@ -1,21 +1,45 @@
 // swagger.ts
-import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { Express } from "express";
+import YAML from 'yamljs';
+import path from "path";
+import { glob } from "glob";
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Smart Home API",
-      version: "1.0.0",
-    },
-  },
-  apis: ["./app/presentation/express/routers/**/*.ts"],
-};
+// Load the OpenAPI document
+const openapiDocument = YAML.load(path.join(__dirname, './openapi.yml'));
 
-const specs = swaggerJsdoc(options);
+// Construct the absolute path to the paths directory
+const pathsDir = path.resolve(__dirname, './paths');
+
+// Construct the absolute path to the schemas directory
+const schemasDir = path.resolve(__dirname, './schemas');
+
+// Use glob to find all YAML files in the paths directory
+const pathsFiles = glob.sync(`${pathsDir}/**/*.yaml`);
+
+if (pathsFiles.length === 0) {
+  console.error('No paths files found!');
+}
+
+// Use glob to find all YAML files in the schemas directory
+const schemasFiles = glob.sync(`${schemasDir}/**/*.yaml`);
+
+if (schemasFiles.length === 0) {
+  console.error('No schemas files found!');
+}
+
+// Load and merge all paths files into the base OpenAPI document
+pathsFiles.forEach(file => {
+  const paths = YAML.load(file);
+  openapiDocument.paths = { ...openapiDocument.paths, ...paths };
+});
+
+// Load and merge all schemas files into the base OpenAPI document
+schemasFiles.forEach(file => {
+  const schemas = YAML.load(file);
+  openapiDocument.components.schemas = { ...openapiDocument.components.schemas, ...schemas };
+});
 
 export const setupSwagger = (app: Express) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiDocument));
 };
--- app/backend/app/presentation/express/routers/restaurant/ingridient.ts
+++ app/backend/app/presentation/express/routers/restaurant/ingridient.ts
@@ -11,269 +11,26 @@ import { deleteIngridientComposer } from "@/restaurant/infra/http/composers/ingr
  */
 const ingridientRoutes = Router();
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     IngridientGetSchema:
- *       type: object
- *       required:
- *         - name
- *         - quantity
- *         - apiUri
- *       properties:
- *         _id:
- *           type: string
- *           description: The auto-generated id of the ingridient
- *         name:
- *           type: string
- *           description: The ingridient's name
- *         quantity:
- *           type: number
- *           description: Quantity of the bought ingridient
- *         apiUri:
- *           type: string
- *           description: API uri to shop where to buy the ingridient
- * 
- *     IngridientPostSchema:
- *       type: object
- *       required:
- *         - name
- *         - quantity
- *         - apiUri
- *       properties:
- *         name:
- *           type: string
- *           description: The ingridient's name
- *         quantity:
- *           type: number
- *           description: Quantity of the bought ingridient
- *         apiUri:
- *           type: string
- *           description: API uri to shop where to buy the ingridient
- *         unit:
- *           type: string
- *           description: The unit the ingridient uses (Kg, L, etc)
- * 
- *     IngridientPutSchema:
- *       type: object
- *       properties:
- *         name:
- *           type: string
- *           description: The ingridient's name
- *         quantity:
- *           type: number
- *           description: Quantity of the bought ingridient
- *         apiUri:
- *           type: string
- *           description: API uri to shop where to buy the ingridient
- *         unit:
- *           type: string
- *           description: The unit the ingridient uses (Kg, L, etc)
- *
- */
-
-/**
- * @swagger
- * /ingridient:
- *   post:
- *     summary: Create a new ingridient
- *     tags: [Ingridients]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/IngridientPostSchema'
- *     responses:
- *       201:
- *         description: The ingridient was successfully created
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/IngridientGetSchema'
- *       500:
- *         description: Some server error
- */
 ingridientRoutes.post("/", async (request: Request, response: Response) => {
-  console.log("Creating ingridient")
-  console.log(request.body)
   const adapter = await expressAdapter(request, createIngridientComposer());
   return response.status(adapter.statusCode).json(adapter.body);
 });
-
-/**
- * @swagger
- * /ingridient/all:
- *   get:
- *     summary: Retrieves all ingridients
- *     tags: [Ingridients]
- *     parameters:
- *       - in: query
- *         name: page
- *         schema:
- *           type: number
- *         description: page number to be shown
- *     responses:
- *       201:
- *         description: The ingridient was successfully created
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/IngridientGetSchema'
- *       500:
- *         description: Some server error
- */
 ingridientRoutes.get("/all", async (request: Request, response: Response) => {
   const adapter = await expressAdapter(request, getAllIngridientComposer());
   return response.status(adapter.statusCode).json(adapter.body);
 });
-
-/**
- * @swagger
- * /ingridient/id/{id}:
- *   get:
- *     summary: Get ingridient by Id
- *     tags: [Ingridients]
- *     parameters:
- *       - in: path
- *         name: id
- *         description: Ingridient's id,
- *         required: true,
- *         schema:
- *           type: string
- *     responses:
- *       400:
- *         description: Ingridient does not exits!,
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *               example:
- *                 error: Ingridient does not exits!
- *       200:
- *         description: The ingridient was found
- *         content:
- *           application/json:
- *             schema:
- *               $ref: "#/components/schemas/IngridientGetSchema"
- *       500:
- *         description: Some server error
- */
 ingridientRoutes.get("/id/:id", async (request: Request, response: Response) => {
   const adapter = await expressAdapter(request, getIngridientByIdComposer());
   return response.status(adapter.statusCode).json(adapter.body);
 });
-
-/**
- * @swagger
- * /ingridient/name/{name}:
- *   get:
- *     summary: Get ingridient by Name
- *     tags: [Ingridients]
- *     parameters:
- *       - in: path
- *         name: name
- *         description: Ingridient's name,
- *         required: true,
- *         schema:
- *           type: string
- *     responses:
- *       400:
- *         description: Ingridient does not exits!,
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *               example:
- *                 error: Ingridient does not exits!
- *       200:
- *         description: The ingridient was found
- *         content:
- *           application/json:
- *             schema:
- *               $ref: "#/components/schemas/IngridientGetSchema"
- *       500:
- *         description: Some server error
- */
 ingridientRoutes.get("/name/:name", async (request: Request, response: Response) => {
   const adapter = await expressAdapter(request, getIngridientByNameComposer());
   return response.status(adapter.statusCode).json(adapter.body);
 });
-
-/**
- * @swagger
- * /ingridient/id/{id}:
- *   patch:
- *     summary: Update ingridient
- *     tags: [Ingridients]
- *     parameters:
- *       - in: path
- *         name: id
- *         description: Ingridient's id for update,
- *         required: true,
- *         schema:
- *           type: string
- *     requestBody:
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/IngridientPutSchema"
- *     responses:
- *       400:
- *         description: Ingridient does not exits!,
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *               example:
- *                 error: Ingridient does not exits!
- *       200:
- *         description: The ingridient was successfully updated
- *         content:
- *           application/json:
- *             schema:
- *               $ref: "#/components/schemas/IngridientGetSchema"
- *       500:
- *         description: Some server error
- */
 ingridientRoutes.patch("/id/:id", async (request: Request, response: Response) => {
   const adapter = await expressAdapter(request, updateIngridientComposer());
   return response.status(adapter.statusCode).json(adapter.body);
 });
-
-/**
- * @swagger
- * /ingridient/id/{id}:
- *   delete:
- *     summary: Delete ingridient
- *     tags: [Ingridients]
- *     parameters:
- *       - in: path
- *         name: id
- *         description: Ingridient's id for delete,
- *         required: true,
- *         schema:
- *           type: string
- *     responses:
- *       201:
- *         description: The ingridient was successfully deleted
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/IngridientGetSchema'
- *       500:
- *         description: Some server error
- */
 ingridientRoutes.delete("/id/:id", async (request: Request, response: Response) => {
   const adapter = await expressAdapter(request, deleteIngridientComposer());
   return response.status(adapter.statusCode).json(adapter.body);
