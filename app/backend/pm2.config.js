172de615910b64155ddc38eec52b03c490837c7a
Marc Lopez <marclopezgarcia@gmail.com>
2024-06-14 16:41:02 +0200

    changed pm2 config

--- app/backend/pm2.config.js
+++ app/backend/pm2.config.js
@@ -1,9 +1,19 @@
 module.exports = {
-    apps: [
-        {
-            name: 'Smart Home Project',
-            script: './dist/app/presentation/express/settings/server.js',
-            node_args: '-r ts-node/register -r tsconfig-paths/register'
-        },
-    ],
+  apps: [
+    {
+      name: "Smart Home Project",
+      script: "./dist/app/presentation/express/settings/server.js",
+      node_args: "-r ts-node/register -r tsconfig-paths/register",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 };
